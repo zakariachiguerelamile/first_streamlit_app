0eb1bf187077aa9cdcea6446cf008d0b2103ceb3
zakaria chiguer <108577661+zakariachiguerelamile@users.noreply.github.com>
2022-07-02 15:28:15 +0200

    Update streamlit_app.py
    fixing the error

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.text('Hard-boiled Free-Range Egg')
 streamlit.text('Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.dataframe(my_fruit_list)
 
@@ -18,4 +19,4 @@ streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-my_fruit_list = my_fruit_list.set_index('Fruit')
+
