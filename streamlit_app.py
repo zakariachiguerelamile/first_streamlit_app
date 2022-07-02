47a484593e4c389dec3a44a621c79b0061446b6c
zakaria chiguer <108577661+zakariachiguerelamile@users.noreply.github.com>
2022-07-02 15:10:45 +0200

    Update streamlit_app.py
    added menu description

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Beakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-boiled Free-Range Egg')
